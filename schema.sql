f8512076ae0c566172b88bce3705ae59f182bd20
Scott Nelson <snelson.seattle@gmail.com>
2020-11-18 22:54:08 -0800

    Fixed schema.sql file

--- schema.sql
+++ schema.sql
@@ -4,17 +4,17 @@ CREATE DATABASE crm_DB;
 
 USE crm_DB;
 
-CREATE TABLE department {
+CREATE TABLE departments {
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 };
 
-CREATE TABLE role (
+CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary  DECIMAL(10,2) NULL,
-    department_id FOREIGN KEY,
+    FOREIGN KEY (department_id) REFERENCES departments(id),
     PRIMARY KEY (id) 
 );
  
@@ -24,7 +24,7 @@ CREATE TABLE employees(
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT FOREIGN KEY,
-    manager_id INT FOREIGN KEY,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
     PRIMARY KEY (id)
 );
